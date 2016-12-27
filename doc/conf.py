3c55305446f8ef5fa21edc6d9bee2393119b5d49
Sebastian Pölsterl <sebp@k-d-w.org>
2016-12-27 22:40:16 +0000

    DOC: Import versioneer after changing sys.path

--- doc/conf.py
+++ doc/conf.py
@@ -14,12 +14,13 @@
 
 import sys
 import os
-import versioneer
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath(os.path.pardir))
+sys.path.insert(0, os.path.abspath(os.path.pardir))
+
+import versioneer
 
 # -- General configuration ------------------------------------------------
 
