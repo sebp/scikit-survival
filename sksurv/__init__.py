f86b632389ebb7e0f486bda630f884be1aaa476c
Sebastian Pölsterl <sebp@k-d-w.org>
2016-12-29 13:02:33 +0000

    Fix indentation

--- sksurv/__init__.py
+++ sksurv/__init__.py
@@ -3,5 +3,5 @@ from pkg_resources import get_distribution, DistributionNotFound
 try:
     __version__ = get_distribution('scikit-survival').version
 except DistributionNotFound:
-   # package is not installed
-   __version__ = 'unknown'
+    # package is not installed
+    __version__ = 'unknown'
