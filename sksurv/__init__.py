dc3e346fc73ddabcbbec1ff47122e89ceecf0b9c
Sebastian Pölsterl <sebp@k-d-w.org>
2018-07-10 21:49:39 +0200

    Exclude unknown __version__ from coverage

--- sksurv/__init__.py
+++ sksurv/__init__.py
@@ -2,6 +2,6 @@ from pkg_resources import get_distribution, DistributionNotFound
 
 try:
     __version__ = get_distribution('scikit-survival').version
-except DistributionNotFound:
+except DistributionNotFound:  # pragma: no cover
     # package is not installed
     __version__ = 'unknown'
