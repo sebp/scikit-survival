79f5c9e59c166167d493800596bae969a5f01d51
Sebastian Pölsterl <sebp@k-d-w.org>
2018-11-29 19:16:23 +0100

    Add codecov config
    - Fix paths
    - Only submit tests/coverage.xml

--- ci/submit_coverage.sh
+++ ci/submit_coverage.sh
@@ -3,7 +3,7 @@ set -xe
 
 if [ "x$NO_SLOW" = "xfalse" ]
 then
-  bash <(curl -s https://codecov.io/bash)
+  bash <(curl -s https://codecov.io/bash) -f tests/coverage.xml
   pip install codacy-coverage
   python-codacy-coverage -r tests/coverage.xml
 fi
