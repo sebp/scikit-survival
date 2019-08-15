sebp/scikit-survival

commit 23fe263e110c7abb68099b48de3a06079e1e6cda

Add predict_survival_function and predict_cumulative_hazard_function to CoxnetSurvivalAnalysis
Estimating the baseline cumulative hazard function requires
access to estimated coefficients and survival times from the
training data. While `predict` allows specifying arbitrary
`alpha` values by interpolating coefficients, we can't do
this for `predict_survival_function` and
`predict_cumulative_hazard_function` without keeping a copy
of the training data. Therefore, both functions only accept
alpha values that have been used during fitting and are
stored in `alphas_`. To avoid increasing training time,
estimating the baseline cumulative hazard function is
disabled by default, set `fit_baseline_model = True` if you
need it.

Fixes #46

sksurv/linear_model/coxph.py
@@ -27,6 +27,103 @@ from ..util import check_arrays_survival
 __all__ = ['CoxPHSurvivalAnalysis']
 
 
+class BreslowEstimator:
+    """Breslow's estimator of the cumulative hazard function.
+
+    Attributes
+    ----------
+    cum_baseline_hazard_ : :class:`sksurv.functions.StepFunction`
+        Cumulative baseline hazard function.
+
+    baseline_survival_ : :class:`sksurv.functions.StepFunction`
+        Baseline survival function.
+    """
+
+    def fit(self, linear_predictor, event, time):
+        """Compute baseline cumulative hazard function.
+
+        Parameters
+        ----------
+        linear_predictor : array-like, shape = (n_samples,)
+            Linear predictor of risk: `X @ coef`.
+
+        event : array-like, shape = (n_samples,)
+            Contains binary event indicators.
+
+        time : array-like, shape = (n_samples,)
+            Contains event/censoring times.
+
+        Returns
+        -------
+        self
+        """
+        risk_score = numpy.exp(linear_predictor)
+        order = numpy.argsort(time, kind="mergesort")
+        risk_score = risk_score[order]
+        uniq_times, n_events, n_at_risk = _compute_counts(event, time, order)
+
+        divisor = numpy.empty(n_at_risk.shape, dtype=numpy.float_)
+        value = numpy.sum(risk_score)
+        divisor[0] = value
+        k = 0
+        for i in range(1, len(n_at_risk)):
+            d = n_at_risk[i - 1] - n_at_risk[i]
+            value -= risk_score[k:(k + d)].sum()
+            k += d
+            divisor[i] = value
+
+        assert k == n_at_risk[0] - n_at_risk[-1]
+
+        y = numpy.cumsum(n_events / divisor)
+        self.cum_baseline_hazard_ = StepFunction(uniq_times, y)
+        self.baseline_survival_ = StepFunction(self.cum_baseline_hazard_.x,
+                                               numpy.exp(- self.cum_baseline_hazard_.y))
+        return self
+
+    def get_cumulative_hazard_function(self, linear_predictor):
+        """Predict cumulative hazard function.
+
+        Parameters
+        ----------
+        linear_predictor : array-like, shape = (n_samples,)
+            Linear predictor of risk: `X @ coef`.
+
+        Returns
+        -------
+        cum_hazard : ndarray, shape = (n_samples,)
+            Predicted cumulative hazard functions.
+        """
+        risk_score = numpy.exp(linear_predictor)
+        n_samples = risk_score.shape[0]
+        funcs = numpy.empty(n_samples, dtype=numpy.object_)
+        for i in range(n_samples):
+            funcs[i] = StepFunction(x=self.cum_baseline_hazard_.x,
+                                    y=self.cum_baseline_hazard_.y,
+                                    a=risk_score[i])
+        return funcs
+
+    def get_survival_function(self, linear_predictor):
+        """Predict survival function.
+
+        Parameters
+        ----------
+        linear_predictor : array-like, shape = (n_samples,)
+            Linear predictor of risk: `X @ coef`.
+
+        Returns
+        -------
+        survival : ndarray, shape = (n_samples,)
+            Predicted survival functions.
+        """
+        risk_score = numpy.exp(linear_predictor)
+        n_samples = risk_score.shape[0]
+        funcs = numpy.empty(n_samples, dtype=numpy.object_)
+        for i in range(n_samples):
+            funcs[i] = StepFunction(x=self.baseline_survival_.x,
+                                    y=numpy.power(self.baseline_survival_.y, risk_score[i]))
+        return funcs
+
+
 class CoxPHOptimizer:
     """Negative partial log-likelihood of Cox proportional hazards model"""
 
@@ -190,6 +287,8 @@ class CoxPHSurvivalAnalysis(BaseEstimator, SurvivalAnalysisMixin):
         self.tol = tol
         self.verbose = verbose
 
+        self._baseline_model = BreslowEstimator()
+
     def fit(self, X, y):
         """Minimize negative partial log-likelihood for provided data.
 
@@ -256,10 +355,7 @@ class CoxPHSurvivalAnalysis(BaseEstimator, SurvivalAnalysisMixin):
             i += 1
 
         self.coef_ = w
-        self.cum_baseline_hazard_ = self._fit_baseline_hazard_function(X, event, time)
-        self.baseline_survival_ = StepFunction(self.cum_baseline_hazard_.x,
-                                               numpy.exp(- self.cum_baseline_hazard_.y))
-
+        self._baseline_model.fit(numpy.dot(X, self.coef_), event, time)
         return self
 
     def predict(self, X):
@@ -304,14 +400,7 @@ class CoxPHSurvivalAnalysis(BaseEstimator, SurvivalAnalysisMixin):
         cum_hazard : ndarray, shape = (n_samples,)
             Predicted cumulative hazard functions.
         """
-        risk_score = numpy.exp(self.predict(X))
-        n_samples = risk_score.shape[0]
-        funcs = numpy.empty(n_samples, dtype=numpy.object_)
-        for i in range(n_samples):
-            funcs[i] = StepFunction(x=self.cum_baseline_hazard_.x,
-                                    y=self.cum_baseline_hazard_.y,
-                                    a=risk_score[i])
-        return funcs
+        return self._baseline_model.get_cumulative_hazard_function(self.predict(X))
 
     def predict_survival_function(self, X):
         """Predict survival function.
@@ -336,51 +425,4 @@ class CoxPHSurvivalAnalysis(BaseEstimator, SurvivalAnalysisMixin):
         survival : ndarray, shape = (n_samples,)
             Predicted survival functions.
         """
-        risk_score = numpy.exp(self.predict(X))
-        n_samples = risk_score.shape[0]
-        funcs = numpy.empty(n_samples, dtype=numpy.object_)
-        for i in range(n_samples):
-            funcs[i] = StepFunction(x=self.baseline_survival_.x,
-                                    y=numpy.power(self.baseline_survival_.y, risk_score[i]))
-        return funcs
-
-    def _fit_baseline_hazard_function(self, X, event, time):
-        """Compute baseline cumulative hazard function.
-
-        Uses Breslow's estimator.
-
-        Parameters
-        ----------
-        X : array-like, shape = (n_samples, n_features)
-            Data matrix.
-
-        event : array-like, shape = (n_samples,)
-            Contains binary event indicators.
-
-        time : array-like, shape = (n_samples,)
-            Contains event/censoring times.
-
-        Returns
-        -------
-        cum_baseline_hazard: :class:`sksurv.functions.StepFunction`
-            Cumulative baseline hazard function.
-        """
-        risk_score = numpy.exp(numpy.dot(X, self.coef_))
-        order = numpy.argsort(time, kind="mergesort")
-        risk_score = risk_score[order]
-        uniq_times, n_events, n_at_risk = _compute_counts(event, time, order)
-
-        divisor = numpy.empty(n_at_risk.shape, dtype=numpy.float_)
-        value = numpy.sum(risk_score)
-        divisor[0] = value
-        k = 0
-        for i in range(1, len(n_at_risk)):
-            d = n_at_risk[i - 1] - n_at_risk[i]
-            value -= risk_score[k:(k + d)].sum()
-            k += d
-            divisor[i] = value
-
-        assert k == n_at_risk[0] - n_at_risk[-1]
-
-        y = numpy.cumsum(n_events / divisor)
-        return StepFunction(uniq_times, y)
+        return self._baseline_model.get_survival_function(self.predict(X))

tests/test_coxph.py
@@ -137,7 +137,7 @@ class TestCoxPH(object):
         expected_x = numpy.array(
             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 30,
              31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 42, 43, 44, 45, 46, 47, 48, 49, 50, 52])
-        assert_array_almost_equal(cph.cum_baseline_hazard_.x, expected_x)
+        assert_array_almost_equal(cph._baseline_model.cum_baseline_hazard_.x, expected_x)
 
         expected_y = numpy.array(
             [0.00678640369024364, 0.0135929334270716, 0.0204043079886091, 0.0272294776707967, 0.0340761479284598,
@@ -151,7 +151,7 @@ class TestCoxPH(object):
              0.72608698374096, 0.744888154417096, 0.763829951751727, 0.802133842428817, 0.811813515937835,
              0.831261170527727, 0.880363253205648, 0.910240767958261, 0.950727380604515])
 
-        actual_y = [cph.cum_baseline_hazard_(v) for v in expected_x]
+        actual_y = [cph._baseline_model.cum_baseline_hazard_(v) for v in expected_x]
         # check that values increase
         assert (numpy.diff(actual_y) > 0).all()
         assert_array_almost_equal(actual_y, expected_y)

sksurv/linear_model/coxnet.py
@@ -20,6 +20,7 @@ from sklearn.utils.validation import assert_all_finite, check_array, check_is_fi
 
 from ..base import SurvivalAnalysisMixin
 from ..util import check_arrays_survival
+from .coxph import BreslowEstimator
 from ._coxnet import call_fit_coxnet
 
 __all__ = ['CoxnetSurvivalAnalysis']
@@ -79,6 +80,13 @@ class CoxnetSurvivalAnalysis(BaseEstimator, SurvivalAnalysisMixin):
     verbose : bool, optional, default: False
         Whether to print additional information during optimization.
 
+    fit_baseline_model : bool, optional, default: False
+        Whether to estimate baseline survival function
+        and baseline cumulative hazard function for each alpha.
+        If enabled, :meth:`predict_cumulative_hazard_function` and
+        :meth:`predict_survival_function` can be used to obtain
+        predicted  cumulative hazard function and survival function.
+
     Attributes
     ----------
     alphas_ : ndarray, shape=(n_alphas,)
@@ -102,7 +110,7 @@ class CoxnetSurvivalAnalysis(BaseEstimator, SurvivalAnalysisMixin):
 
     def __init__(self, n_alphas=100, alphas=None, alpha_min_ratio=0.0001, l1_ratio=0.5,
                  penalty_factor=None, normalize=False, copy_X=True,
-                 tol=1e-7, max_iter=100000, verbose=False):
+                 tol=1e-7, max_iter=100000, verbose=False, fit_baseline_model=False):
         self.n_alphas = n_alphas
         self.alphas = alphas
         self.alpha_min_ratio = alpha_min_ratio
@@ -113,6 +121,9 @@ class CoxnetSurvivalAnalysis(BaseEstimator, SurvivalAnalysisMixin):
         self.tol = tol
         self.max_iter = max_iter
         self.verbose = verbose
+        self.fit_baseline_model = fit_baseline_model
+
+        self._baseline_models = None
 
     def _pre_fit(self, X, y):
         X, event, time = check_arrays_survival(X, y, copy=self.copy_X)
@@ -202,6 +213,14 @@ class CoxnetSurvivalAnalysis(BaseEstimator, SurvivalAnalysisMixin):
                           category=ConvergenceWarning,
                           stacklevel=2)
 
+        if self.fit_baseline_model:
+            self._baseline_models = tuple(
+                BreslowEstimator().fit(numpy.dot(X, coef[:, i]), event_num, time)
+                for i in range(coef.shape[1])
+            )
+        else:
+            self._baseline_models = None
+
         self.alphas_ = alphas
         self.penalty_factor_ = penalty
         self.coef_ = coef
@@ -264,3 +283,86 @@ class CoxnetSurvivalAnalysis(BaseEstimator, SurvivalAnalysisMixin):
         X = check_array(X)
         coef = self._get_coef(alpha)
         return numpy.dot(X, coef)
+
+    def _get_baseline_model(self, alpha):
+        check_is_fitted(self, "coef_")
+        if self._baseline_models is None:
+            raise ValueError('`fit` must be called with the fit_baseline_model option set to True.')
+
+        if alpha is None:
+            baseline_model = self._baseline_models[-1]
+        else:
+            is_close = numpy.isclose(alpha, self.alphas_)
+            if is_close.any():
+                idx = numpy.flatnonzero(is_close)[0]
+                baseline_model = self._baseline_models[idx]
+            else:
+                raise ValueError('alpha must be one value of alphas_: %s' % self.alphas_)
+
+        return baseline_model
+
+    def predict_cumulative_hazard_function(self, X, alpha=None):
+        """Predict cumulative hazard function.
+
+        Only available if :meth:`fit` has been called with `fit_baseline_model = True`.
+
+        The cumulative hazard function for an individual
+        with feature vector :math:`x_\\alpha` is defined as
+
+        .. math::
+
+            H(t \\mid x_\\alpha) = \\exp(x_\\alpha^\\top \\beta) H_0(t) ,
+
+        where :math:`H_0(t)` is the baseline hazard function,
+        estimated by Breslow's estimator.
+
+        Parameters
+        ----------
+        X : array-like, shape = (n_samples, n_features)
+            Data matrix.
+
+        alpha : float, optional
+            Constant that multiplies the penalty terms. The same alpha as used during training
+            must be specified. If set to ``None``, the last alpha in the solution path is used.
+
+        Returns
+        -------
+        cum_hazard : ndarray, shape = (n_samples,)
+            Predicted cumulative hazard functions.
+        """
+        baseline_model = self._get_baseline_model(alpha)
+
+        return baseline_model.get_cumulative_hazard_function(self.predict(X, alpha=alpha))
+
+    def predict_survival_function(self, X, alpha=None):
+        """Predict survival function.
+
+        Only available if :meth:`fit` has been called with `fit_baseline_model = True`.
+
+        The survival function for an individual
+        with feature vector :math:`x_\\alpha` is defined as
+
+        .. math::
+
+            S(t \\mid x_\\alpha) = S_0(t)^{\\exp(x_\\alpha^\\top \\beta)} ,
+
+        where :math:`S_0(t)` is the baseline survival function,
+        estimated by Breslow's estimator.
+
+        Parameters
+        ----------
+        X : array-like, shape = (n_samples, n_features)
+            Data matrix.
+
+        alpha : float, optional
+            Constant that multiplies the penalty terms. The same alpha as used during training
+            must be specified. If set to ``None``, the last alpha in the solution path is used.
+
+        Returns
+        -------
+        survival : ndarray, shape = (n_samples,)
+            Predicted survival functions.
+        """
+        baseline_model = self._get_baseline_model(alpha)
+
+        return baseline_model.get_survival_function(self.predict(X, alpha=alpha))

tests/test_coxnet.py
@@ -425,6 +425,36 @@ class TestCoxnetSurvivalAnalysis(object):
             coxnet.predict(x.iloc[[122, 10, 22, 200], :], alpha=a) for a in [0.75, 0.25, 0.2, 0.15, 0.1, 0.075]])
         assert_array_almost_equal(pred, expected_pred)
 
+    def test_example_2_predict_func(self):
+        x, coxnet = self._fit_example(l1_ratio=0.9, n_alphas=11,
+                                      alpha_min_ratio=0.001,
+                                      fit_baseline_model=True)
+
+        xtest = x.iloc[[122, 10, 22, 200], :]
+        for a in [None] + list(coxnet.alphas_):
+            chf = coxnet.predict_cumulative_hazard_function(xtest, alpha=a)
+            assert len(chf) == 4
+
+            sf = coxnet.predict_survival_function(xtest, alpha=a)
+            assert len(sf) == 4
+
+    def test_predict_func_disabled(self):
+        x, coxnet = self._fit_example(l1_ratio=0.9, n_alphas=11,
+                                      alpha_min_ratio=0.001,
+                                      fit_baseline_model=False)
+        with pytest.raises(ValueError,
+                           match='`fit` must be called with the fit_baseline_model option set to True.'):
+            coxnet.predict_cumulative_hazard_function(x)
+
+    def test_predict_func_no_such_alpha(self):
+        x, coxnet = self._fit_example(l1_ratio=0.9, n_alphas=11,
+                                      alpha_min_ratio=0.001,
+                                      fit_baseline_model=True)
+        with pytest.raises(ValueError,
+                           match=r'alpha must be one value of alphas_: \[.+'):
+            for a in 1. + numpy.random.randn(100):
+                coxnet.predict_cumulative_hazard_function(x, alpha=a)
+
     def test_all_zero_coefs(self):
         alphas = numpy.array([256, 128, 96, 64, 48])
 

commit 45e7d756ac09e6227b085344d64af79defe1b330

Re-add baseline_survival_ and cum_baseline_hazard_ to CoxPHSurvivalAnalysis
Fixes a regression that was introduced in commit
23fe263e110c7abb68099b48de3a06079e1e6cda

Fixes #76

sksurv/linear_model/coxph.py
@@ -291,6 +291,14 @@ class CoxPHSurvivalAnalysis(BaseEstimator, SurvivalAnalysisMixin):
 
         self._baseline_model = BreslowEstimator()
 
+    @property
+    def cum_baseline_hazard_(self):
+        return self._baseline_model.cum_baseline_hazard_
+
+    @property
+    def baseline_survival_(self):
+        return self._baseline_model.baseline_survival_
+
     def fit(self, X, y):
         """Minimize negative partial log-likelihood for provided data.
 

tests/test_coxph.py
@@ -137,7 +137,7 @@ class TestCoxPH(object):
         expected_x = numpy.array(
             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 30,
              31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 42, 43, 44, 45, 46, 47, 48, 49, 50, 52])
-        assert_array_almost_equal(cph._baseline_model.cum_baseline_hazard_.x, expected_x)
+        assert_array_almost_equal(cph.cum_baseline_hazard_.x, expected_x)
 
         expected_y = numpy.array(
             [0.00678640369024364, 0.0135929334270716, 0.0204043079886091, 0.0272294776707967, 0.0340761479284598,
@@ -151,7 +151,7 @@ class TestCoxPH(object):
              0.72608698374096, 0.744888154417096, 0.763829951751727, 0.802133842428817, 0.811813515937835,
              0.831261170527727, 0.880363253205648, 0.910240767958261, 0.950727380604515])
 
-        actual_y = [cph._baseline_model.cum_baseline_hazard_(v) for v in expected_x]
+        actual_y = [cph.cum_baseline_hazard_(v) for v in expected_x]
         # check that values increase
         assert (numpy.diff(actual_y) > 0).all()
         assert_array_almost_equal(actual_y, expected_y)