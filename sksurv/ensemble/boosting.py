6e8483068abf52bd3eca795880f24260e357a669
Georgios Kaissis <48105485+gkaissis@users.noreply.github.com>
2019-08-22 07:22:18 +0200

    Update boosting.py

--- sksurv/ensemble/boosting.py
+++ sksurv/ensemble/boosting.py
@@ -482,7 +482,7 @@ class GradientBoostingSurvivalAnalysis(BaseGradientBoosting, SurvivalAnalysisMix
         Choosing `subsample < 1.0` leads to a reduction of variance
         and an increase in bias.
         
-    dropout_rate : float, optional, default: 0.0
+        dropout_rate : float, optional, default: 0.0
         If larger than zero, the residuals at each iteration are only computed
         from a random subset of base learners. The value corresponds to the
         percentage of base learners that are dropped. In each iteration,
