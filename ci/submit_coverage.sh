069512091c02d38554fe193a805cbd70a340e7ba
Sebastian Pölsterl <sebp@k-d-w.org>
2020-04-10 19:06:05 +0200

    Remove Travic CI config

--- ci/submit_coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -xe
-
-if [ "x$NO_SLOW" = "xfalse" ]
-then
-  bash <(curl -s https://codecov.io/bash) -f tests/coverage.xml
-  pip install codacy-coverage
-  python-codacy-coverage -r tests/coverage.xml
-fi
