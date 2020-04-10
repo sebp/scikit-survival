8c5dbb4f27a56887c2de5857e6ab95752d38e027
Sebastian Pölsterl <sebp@k-d-w.org>
2020-04-10 19:29:26 +0200

    Allow pandas 1.0
    Closes #100

--- ci/deps/py38_pandas10_numpy118_sklearn022.sh
+++ ci/deps/py38_pandas10_numpy118_sklearn022.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=sh
 export CONDA_PYTHON_VERSION=3.8
-export PANDAS_VERSION=0.25
+export PANDAS_VERSION=1.0
 export NUMPY_VERSION=1.18
 export SKLEARN_VERSION=0.22
-export NO_SLOW=true
+export NO_SLOW=false
