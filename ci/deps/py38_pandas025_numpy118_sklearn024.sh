947bc8958b63988ac4ba998377204f81b5364f03
Sebastian Pölsterl <sebp@k-d-w.org>
2021-10-28 09:27:54 +0200

    CI: Test with pandas 1.3 and numpy 1.21

--- ci/deps/py38_pandas025_numpy118_sklearn024.sh
+++
@@ -1,6 +0,0 @@
-# shellcheck shell=sh
-export CONDA_PYTHON_VERSION='3.8.*'
-export PANDAS_VERSION='0.25.*'
-export NUMPY_VERSION='1.18.*'
-export SKLEARN_VERSION='0.24.*'
-export NO_SLOW=true
--- ci/deps/py39_pandas13_numpy121_sklearn024.sh
+++ ci/deps/py39_pandas13_numpy121_sklearn024.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=sh
 export CONDA_PYTHON_VERSION='3.9.*'
-export PANDAS_VERSION='1.2.*'
-export NUMPY_VERSION='1.20.*'
+export PANDAS_VERSION='1.3.*'
+export NUMPY_VERSION='1.21.*'
 export SKLEARN_VERSION='0.24.*'
-export NO_SLOW=true
+export NO_SLOW=false
