7104cd8f5278b72ca31b2576ae0ea2f4ef0fd296
Sebastian Pölsterl <sebp@k-d-w.org>
2022-04-15 19:34:25 +0200

    CI: Use conda with mambda solver

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -12,9 +12,12 @@ else
   exit 1
 fi
 
+conda update -q conda
 conda config --set always_yes yes --set changeps1 no
 
-conda create -n sksurv-test \
+conda install -n base conda-libmamba-solver
+
+conda create -n sksurv-test --experimental-solver=libmamba \
   python="${CONDA_PYTHON_VERSION:?}" \
   numpy="${NUMPY_VERSION:?}" \
   pandas="${PANDAS_VERSION:?}" \
