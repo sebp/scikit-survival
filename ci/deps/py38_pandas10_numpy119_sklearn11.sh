348c589e56a2bd2dad19335b81dcbbc6f56ed97d
Sebastian Pölsterl <sebp@k-d-w.org>
2022-08-02 20:28:44 +0200

    Require scikit-learn 1.1

--- ci/deps/py38_pandas10_numpy119_sklearn11.sh
+++ ci/deps/py38_pandas10_numpy119_sklearn11.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=sh
 export CONDA_PYTHON_VERSION='3.8.*'
 export PANDAS_VERSION='1.0.*'
-export NUMPY_VERSION='1.18.*'
-export SKLEARN_VERSION='1.0.*'
-export NO_SLOW=true
+export NUMPY_VERSION='1.19.*'
+export SKLEARN_VERSION='1.1.*'
+export NO_SLOW=false
--- doc/install.rst
+++ doc/install.rst
@@ -91,6 +91,6 @@ The current minimum dependencies to run scikit-survival are:
 - numpy 1.16 or later
 - osqp
 - pandas 0.25 or later
-- scikit-learn 1.0
+- scikit-learn 1.1
 - scipy 1.0 or later
 - C/C++ compiler
