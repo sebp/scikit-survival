62baceea4956062ddce7acc3fb32c7f323fec410
Sebastian Pölsterl <sebp@k-d-w.org>
2023-01-22 17:25:10 +0100

    Drop normalize parameter
    Has been removed it sklearn 1.2

--- sksurv/linear_model/aft.py
+++ sksurv/linear_model/aft.py
@@ -58,11 +58,10 @@ class IPCRidge(Ridge, SurvivalAnalysisMixin):
            present", Journal of Multivariate Analysis, vol. 45, no. 1, pp. 89-103, 1993.
            doi:10.1006/jmva.1993.1028.
     """
-    def __init__(self, alpha=1.0, fit_intercept=True, normalize="deprecated",
-                 copy_X=True, max_iter=None, tol=1e-3, solver="auto"):
-        super().__init__(alpha=alpha, fit_intercept=fit_intercept,
-                         normalize=normalize, copy_X=copy_X,
-                         max_iter=max_iter, tol=tol, solver=solver)
+    def __init__(self, alpha=1.0, fit_intercept=True, copy_X=True, max_iter=None, tol=1e-3, solver="auto"):
+        super().__init__(
+            alpha=alpha, fit_intercept=fit_intercept, copy_X=copy_X, max_iter=max_iter, tol=tol, solver=solver,
+        )
 
     @property
     def _predict_risk_score(self):
