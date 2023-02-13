8c28b8557ed8d272eb15efc8068b9c5c6ffafd4a
Sebastian Pölsterl <sebp@k-d-w.org>
2023-02-13 21:52:51 +0100

    Add _parameter_constraints to NaiveSurvivalSVM

--- sksurv/svm/naive_survival_svm.py
+++ sksurv/svm/naive_survival_svm.py
@@ -102,6 +102,18 @@ class NaiveSurvivalSVM(SurvivalAnalysisMixin, LinearSVC):
            Bioinformatics 24(14), 1632-8, 2008.
 
     """
+
+    _parameter_constraints = {
+        "penalty": LinearSVC._parameter_constraints["penalty"],
+        "loss": LinearSVC._parameter_constraints["loss"],
+        "dual": LinearSVC._parameter_constraints["dual"],
+        "tol": LinearSVC._parameter_constraints["tol"],
+        "alpha": LinearSVC._parameter_constraints["C"],
+        "verbose": LinearSVC._parameter_constraints["verbose"],
+        "random_state": LinearSVC._parameter_constraints["random_state"],
+        "max_iter": LinearSVC._parameter_constraints["max_iter"],
+    }
+
     def __init__(self, penalty='l2', loss='squared_hinge', dual=False, tol=1e-4,
                  alpha=1.0, verbose=0, random_state=None, max_iter=1000):
         super().__init__(penalty=penalty,
