6cbb8fa13dc3d1e82f10dda7c9071ee011d2c36c
Sebastian Pölsterl <sebp@k-d-w.org>
2023-06-17 21:21:21 +0000

    Fix code format

--- sksurv/tree/tree.py
+++ sksurv/tree/tree.py
@@ -243,9 +243,7 @@ class SurvivalTree(BaseEstimator, SurvivalAnalysisMixin):
 
         # Build tree
         self.criterion = "logrank"
-        criterion = LogrankCriterion(
-            self.n_outputs_, n_samples, self.unique_times_, self.is_event_time_
-        )
+        criterion = LogrankCriterion(self.n_outputs_, n_samples, self.unique_times_, self.is_event_time_)
 
         SPLITTERS = SPARSE_SPLITTERS if issparse(X) else DENSE_SPLITTERS
 
