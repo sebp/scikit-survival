ad17e9cd50cd267e832bc6e757d291ff8f5b7cb2
Sebastian Pölsterl <sebp@k-d-w.org>
2023-08-16 08:20:48 +0200

    CI: Raise numpy versions
    Older versions are no longer supported

--- ci/deps/py38_pandas10_numpy121_sklearn13.sh
+++ ci/deps/py38_pandas10_numpy121_sklearn13.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=sh
 export CI_PYTHON_VERSION='3.8.*'
 export CI_PANDAS_VERSION='1.0.*'
-export CI_NUMPY_VERSION='1.19.*'
+export CI_NUMPY_VERSION='1.21.*'
 export CI_SKLEARN_VERSION='1.3.*'
 export CI_NO_SLOW=false
