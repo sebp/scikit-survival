2b8baf11852b2022eccdd8d2ddad30009c4461fa
Sebastian Pölsterl <sebp@k-d-w.org>
2023-11-30 20:01:53 +0100

    Add support for Python 3.12

--- ci/deps/py311_pandas20_numpy124_sklearn13.sh
+++ ci/deps/py311_pandas20_numpy124_sklearn13.sh
@@ -3,4 +3,4 @@ export CI_PYTHON_VERSION='3.11.*'
 export CI_PANDAS_VERSION='2.0.*'
 export CI_NUMPY_VERSION='1.24.*'
 export CI_SKLEARN_VERSION='1.3.*'
-export CI_NO_SLOW=false
+export CI_NO_SLOW=true
---
+++ ci/deps/py312_pandas21_numpy126_sklearn13.sh
@@ -0,0 +1,6 @@
+# shellcheck shell=sh
+export CI_PYTHON_VERSION='3.12.*'
+export CI_PANDAS_VERSION='2.1.*'
+export CI_NUMPY_VERSION='1.26.*'
+export CI_SKLEARN_VERSION='1.3.*'
+export CI_NO_SLOW=false
