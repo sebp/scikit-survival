024f3bcd65d746018aa5bb0bdc7f213b1eed4a02
Sebastian Pölsterl <sebp@k-d-w.org>
2024-05-29 21:38:39 +0200

    Allow scikit-learn 1.5

--- ci/deps/py311.sh
+++ ci/deps/py311.sh
@@ -2,5 +2,5 @@
 export CI_PYTHON_VERSION='3.11.*'
 export CI_PANDAS_VERSION='2.0.*'
 export CI_NUMPY_VERSION='1.25.*'
-export CI_SKLEARN_VERSION='1.4.*'
+export CI_SKLEARN_VERSION='1.5.*'
 export CI_NO_SLOW=true
