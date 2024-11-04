11bffe182a6586162af21a528a0af224b9f04302
Sebastian Pölsterl <sebp@k-d-w.org>
2024-11-04 15:14:27 +0100

    CI: Run tests for python 3.13

--- ci/deps/py312.sh
+++ ci/deps/py312.sh
@@ -3,4 +3,4 @@ export CI_PYTHON_VERSION='3.12.*'
 export CI_PANDAS_VERSION='2.2.*'
 export CI_NUMPY_VERSION='2.0.*'
 export CI_SKLEARN_VERSION='1.5.*'
-export CI_NO_SLOW=false
+export CI_NO_SLOW=true
---
+++ ci/deps/py313.sh
@@ -0,0 +1,6 @@
+# shellcheck shell=sh
+export CI_PYTHON_VERSION='3.13.*'
+export CI_PANDAS_VERSION='2.2.*'
+export CI_NUMPY_VERSION='2.1.*'
+export CI_SKLEARN_VERSION='1.5.*'
+export CI_NO_SLOW=false
