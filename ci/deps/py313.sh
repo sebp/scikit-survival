8a97071fd7442b221d683c767bddd14cd157b120
Sebastian Pölsterl <sebp@k-d-w.org>
2024-12-21 14:07:53 +0100

    Require scikit-learn 1.6.0

--- ci/deps/py313.sh
+++ ci/deps/py313.sh
@@ -2,5 +2,5 @@
 export CI_PYTHON_VERSION='3.13.*'
 export CI_PANDAS_VERSION='2.2.*'
 export CI_NUMPY_VERSION='2.1.*'
-export CI_SKLEARN_VERSION='1.5.*'
+export CI_SKLEARN_VERSION='1.6.*'
 export CI_NO_SLOW=false
