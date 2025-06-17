efdbda7b3148f74881fd1933a8364409768338dc
Sebastian Pölsterl <sebp@k-d-w.org>
2025-06-17 12:46:34 +0200

    CI: Bump numpy version

--- ci/deps/py312.sh
+++ ci/deps/py312.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=sh
 export CI_PYTHON_VERSION='3.12.*'
 export CI_PANDAS_VERSION='2.2.*'
-export CI_NUMPY_VERSION='2.0.*'
+export CI_NUMPY_VERSION='2.2.*'
 export CI_SKLEARN_VERSION='1.7.*'
 export CI_NO_SLOW=true
